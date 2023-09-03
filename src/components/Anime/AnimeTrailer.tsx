0fd38c09fbe7c6d4e81ffd1e807641f198d5dba5
PhYell <94902895+PhYell@users.noreply.github.com>
2023-09-03 19:26:09 +0400

    removed code that was used to check if the video was avaliable in user's country, because it was not very usefull

--- src/components/Anime/AnimeTrailer.tsx
+++ src/components/Anime/AnimeTrailer.tsx
@@ -10,51 +10,6 @@ const YOUTUBE_API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY
 
 // renders youtube video in frame with given videoID
 const AnimeTrailer = ({ videoId }: Props) => {
-  const [isAvailable, setIsAvailable] = useState<boolean>(true)
-
-  const checkVideoAvailability = async () => {
-    const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=contentDetails&key=${YOUTUBE_API_KEY}`
-    )
-
-    return response.data
-  }
-
-  const { data, isLoading, error } = useQuery({
-    queryFn: checkVideoAvailability,
-    queryKey: ["youtube video", videoId],
-  })
-
-  useEffect(() => {
-    if (
-      !videoId ||
-      isLoading ||
-      error ||
-      !data?.items?.[0].contentDetails?.regionRestriction?.allowed
-    )
-      return
-
-    if (data.items && data.items.length > 0) {
-      const restrictedRegions =
-        data.items[0].contentDetails.regionRestriction.allowed
-
-      // checking user's region, so if this video is blocked in their country it won't show it
-      // its not correct, because it just checks browser language, there is a way to check region by using  API (http://ip-api.com/json) and send user's IP to it to get their region, but I think its not that big of a deal
-      const userLocale = navigator.language || navigator.language
-      const userRegion = new Intl.Locale(userLocale).region
-
-      setIsAvailable(!restrictedRegions.includes(userRegion))
-    }
-  }, [videoId, isLoading, error, data])
-
-  if (!videoId) return <></>
-
-  if (isLoading) return <>Loading...</>
-
-  if (error || !data) return <>something wen't wrong</>
-
-  if (!isAvailable) return <></>
-
   return (
     <section>
       <h2 className="text-sp-black dark:text-white mx-auto max-w-7xl mb-1 text-xl">
